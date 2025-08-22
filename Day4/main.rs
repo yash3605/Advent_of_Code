b2ea3951e33aa8b142b1c9a6121c93ab1acb6a46
yashprt <pratapyashkulshrestha@gmail.com>
2025-08-22 19:19:04 +0530

    AOC: Solved Day4 2015

---
+++ Day4/main.rs
@@ -0,0 +1,25 @@
+use md5;
+use regex::Regex;
+
+fn main() {
+    let secret = "bgvyzdsv";
+    let re = Regex::new(r"^0{5,}").unwrap();
+    let mut counter: u64 = 1;
+
+    loop {
+        let input = format!("{}{}", secret, counter);
+        let digest = format!("{:x}", md5::compute(input));
+
+        if re.is_match(&digest) {
+            println!("Found! Counter: {}, Digest: {:?}", counter, digest);
+            break;
+        }
+        counter += 1;
+    }
+}
+
+/*
+[dependencies]
+"md5" = "0.7"
+"regex" = "1"
+*/
