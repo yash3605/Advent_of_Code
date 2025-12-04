b96d016181e573d242eef1a7cf133e24840dccb6
yashpratap <pratapyashkulshrestha@gmail.com>
2025-12-04 20:02:45 +0530

    2025: Day_02 Part 1 & 2

---
+++ 2025/day_02/part_01/main.rs
@@ -0,0 +1,47 @@
+use std::fs;
+
+fn check_id(id: i64) -> bool {
+    let id_in_string = id.to_string();
+    let mid = id_in_string.len() / 2;
+
+    if id_in_string.len() % 2 != 0 {
+        return true;
+    } else {
+        let (left, right) = id_in_string.split_at(mid);
+
+        if left == right {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+fn main() {
+    let s = fs::read_to_string("input.txt").expect("Failed to read File");
+
+    let mut res_sum = 0;
+
+    let ranges: Vec<(i64, i64)> = s
+        .split(',')
+        .map(|part| {
+            let (a, b) = part.trim().split_once('-').unwrap();
+            (
+                a.trim().parse::<i64>().unwrap(),
+                b.trim().parse::<i64>().unwrap(),
+            )
+        })
+        .collect();
+
+    for i in 0..ranges.len() {
+        for j in ranges[i].0..ranges[i].1 {
+            let is_valid = check_id(j);
+
+            if !is_valid {
+                res_sum += j;
+            }
+        }
+    }
+
+    println!("Sum of Invalid Ids: {}", res_sum);
+}
